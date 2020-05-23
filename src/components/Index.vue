97492b8a8bb44774dfceeea10450054ae5037d03
chenxiaolu123 <305395213@qq.com>
2020-05-23 11:52:22 +0800

    配置自动打开浏览器

---
+++ src/components/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="index">
+    <h1>123</h1>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang='scss' scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // 设置自动打开浏览器
+  devServer: {
+    port: 3000,
+    open: true
+  }
+}
